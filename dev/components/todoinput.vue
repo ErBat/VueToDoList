56758f7a67bcdf108b236fd0c71df33add961bb6
ErBat <tork789@gmail.com>
2018-01-25 14:10:52 +0500

    *CDN version and dev version added

---
+++ dev/components/todoinput.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <input v-model="inputVal">
+        <button v-on:click="addItem(inputVal)">Add item</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return { inputVal: '' }
+    },
+    methods: {
+        addItem: function (value) {
+            store.commit('addItem', value)
+        }
+    }
+}
+</script>
